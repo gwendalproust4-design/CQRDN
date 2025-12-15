0d9050cbf8d9b7f38c419a8c50df8b31839f6b97
Fevrie <gwendalproust4@gmail.com>
2025-12-15 08:05:36 +0100

    fix: update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/stream-funding/',
+  base: '/CQRDN/',
   plugins: [
     react(),
     tailwindcss(),
