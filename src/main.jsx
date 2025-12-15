2c6d4d7028a0f02b2640b18a28b9aede43e3f25a
Fevrie <gwendalproust4@gmail.com>
2025-12-15 09:36:48 +0100

    debug: test ultra-simple React component (no dependencies)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppMinimal from './AppMinimal.jsx'  // TEST: Ultra-simple version
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppMinimal />
   </StrictMode>,
 )
