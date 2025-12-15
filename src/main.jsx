54095586fafe1ebc493bf6195198ce599dd9101d
Fevrie <gwendalproust4@gmail.com>
2025-12-15 09:48:14 +0100

    restore: use App.jsx (real site)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppMinimal from './AppMinimal.jsx'  // TEST: Ultra-simple version
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppMinimal />
+    <App />
   </StrictMode>,
 )
